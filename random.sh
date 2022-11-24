3452fba8dd9b8e78d3b7acd4f9c643e1a9f92cab
shubham07h <shubham1609sharma@gmail.com>
2022-11-24 15:48:30 +0530

    Add Sum of three Integer

--- random.sh
+++ random.sh
@@ -1,59 +1,15 @@
-#store input number
+echo "Enter first number"
+read num1
+echo "Enter second number"
+read num2
+echo "Enter third number"
+read num3
 
-read -p "Enter a number : " num
+sum=$((num1 + num2 + num3))
 
-#declare a blank array
-
-declare -a arr
-
-#initialize variable
-
-counter=0
-
-#extract factors
-
-for (( i=2; i<=num/2; i++ ))
-
-do
-
-if [ $((num%i)) -eq 0 ]
-
+if [[ $sum -eq 0 ]]
 then
-
-flag=1
-
-#check if number is prime or not
-
-for (( j=2; j<=i/2; j++ ))
-
-do
-
-if [ $((i%j)) -eq 0 ]
-
-then
-
-flag=0
-
-break
-
+   echo "Sum is zero"
+else
+   echo "Sum is nonzero"
 fi
-
-done
-
-#if number is prime store it in array
-
-if [ $flag -eq 1 ]
-
-then
-
-arr[((counter++))]=$i
-
-fi
-
-fi
-
-done
-
-#display array
-
-echo ${arr[@]}
